735219156bc1229de0d1dd2f073b396c567eb131
Kelsey Weinman <ksw9906@rit.edu>
2016-11-09 18:25:05 -0500

    code

---
+++ client/maker.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+    const handleError = (message) => {
+        $("#errorMessage").text(message);
+        $("#domoMessage").animate({width:'toggle'},350);
+    }
+    
+    const sendAjax = (action, data) => {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            url: action,
+            data: data,
+            dataType: "json",
+            success: (result, status, xhr) => {
+                $("#domoMessage").animate({width:'hide'},350);
+
+                window.location = result.redirect;
+            },
+            error: (xhr, status, error) => {
+                const messageObj = JSON.parse(xhr.responseText);
+            
+                handleError(messageObj.error);
+            }
+        });        
+    }
+    
+    $("#makeDomoSubmit").on("click", (e) => {
+        e.preventDefault();
+    
+        $("#domoMessage").animate({width:'hide'},350);
+    
+        if($("#domoName").val() == '' || $("#domoAge").val() == '') {
+            handleError("RAWR! All fields are required");
+            return false;
+        }
+
+        sendAjax($("#domoForm").attr("action"), $("#domoForm").serialize());
+        
+        return false;
+    });
+  
+    $("body").on("click", (e) =>{
+      var target = $(e.target);
+//      console.log(target);
+      if(target[0].className === "deleteButton"){
+        var data = {name: target[0].name};
+        $.ajax({
+            cache: false,
+            type: "POST",
+            url: "/remove",
+            data: data,
+            dataType: "json",
+            success: (result, status, xhr) => {
+                console.log("success");
+            },
+            error: (xhr, status, error) => {
+              console.log("error");
+              console.log(error);
+            }
+        });        
+//        console.log('deletebutton');
+      }
+    });
+});
